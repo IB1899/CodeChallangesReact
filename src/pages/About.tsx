fd4cbccbc2c11613f34429c98103fff3d290f608
Ibrahim Ali <lya838021@gmail.com>
2022-12-31 19:24:44 +0300

    Keyboard (not by selectAll)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,43 +1,83 @@
+import { ChangeEvent, useState } from "react";
 
 let About = () => {
 
+    //* The keyboard letters inside an array
+    //* So I display them in the jsx by looping through them
+    let [letters, setLetters] = useState<string[]>([
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s",
+        "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"
+    ])
+
+
+    //* State for holding the displayed letters
+    let [element, setElement] = useState<string>("");
+
+    //* Condition for showing or covering the keyboard
+    //* It triggers after a user clicks on the enter button
+    let [isTrue, setIsTrue] = useState<boolean>(false);
+
+    //? Check if the letters are already uppercase then turn them to lowercase , and reverse
+    function isUpperCase(str: string) {
+
+        //* Returns true if the parameter is uppercase  , and false if it's lowercase
+        return str === str.toUpperCase();
+    }
 
     return (
         <div className="About">
 
-            <div className="keyboard">
-                <div className="letter uppercase">uppercase</div>
-                <div className="letter">q</div>
-                <div className="letter">w</div>
-                <div className="letter">e</div>
-                <div className="letter">r</div>
-                <div className="letter">t</div>
-                <div className="letter">y</div> 
-                <div className="letter">u</div>
-                <div className="letter">i</div>
-                <div className="letter">o</div>
-                <div className="letter">p</div>
-                <div className="letter">a</div>
-                <div className="letter">s</div>
-                <div className="letter">d</div>
-                <div className="letter">f</div>
-                <div className="letter">g</div>
-                <div className="letter">h</div>
-                <div className="letter">j</div>
-                <div className="letter">k</div>
-                <div className="letter">l</div>
-                <div className="letter">z</div>
-                <div className="letter">x</div>
-                <div className="letter">c</div>
-                <div className="letter">v</div>
-                <div className="letter">b</div>
-                <div className="letter">n</div>
-                <div className="letter">m</div>
-                <div className="letter delete">delete</div>
-                <div className="letter space">&nbsp</div>
-                <button className="letter enter">enter</button>
+            <input type="text" value={element} onChange={(e: ChangeEvent<HTMLInputElement>) => setElement(e.target.value)} />
+
+
+            {/* ----------------------keyboard--------------------------- */}
+            <div className={isTrue ? "done" : "keyboard"}>
+
+                {/* //* Turn letters to uppercase and reverse button */}
+                <div className="letter uppercase" onClick={() => {
+                    setLetters(
+                        letters.map((letter) => {
+
+                            //? Check if the letters are already uppercase turn them to lowercase , and reverse
+                            if (isUpperCase(letter)) {
+                                return letter = letter.toLocaleLowerCase()!;
+                            }
+                            else {
+                                return letter = letter.toUpperCase()!;
+                            }
+                        })
+                    )
+                }}>
+                    uppercase
+                </div>
+
+                {/* //* Displaying the letters */}
+                {
+                    letters.map(one => (
+                        <div className="letter" key={one} onClick={() => { setElement(element + one); }}>
+                            {one}
+                        </div>
+                    ))
+                }
+
+                {/* //* delete a letter */}
+                <div className="letter delete" onClick={() => setElement(element.toString().slice(0, -1))}>
+                    delete
+                </div>
+
+                {/* //* Adding space */}
+                <div className="letter space" onClick={() => setElement(element + " ")}>
+                </div>
+
+                {/* // enter button */}
+                <div className="letter enter" onClick={() => { setIsTrue(true) }}>
+                    enter
+                </div>
+
 
             </div>
+            {/* ----------------------keyboard--------------------------- */}
+
 
         </div>
     );
